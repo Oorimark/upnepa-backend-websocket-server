0a9e7a2edddc62366896aae1d100d90a9d49aaf0
Oori Mark <oorimark@gmail.com>
2024-01-25 12:44:34 +0100

    Added to web server

--- index.py
+++ index.py
@@ -20,3 +20,13 @@ def handle_message(msg):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
+
+mode = "prod"
+
+if __name__ == "__main__":
+    if mode == "dev":
+        app.run(debug=True, port=3112)
+    else:
+        serve(socketio, host="0.0.0.0", port=4020, threads=10)
+        # socketio.run(app, debug=True, host='0.0.0.0', port=5000)
